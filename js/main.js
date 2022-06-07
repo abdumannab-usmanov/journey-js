db51f6c65b3e386873d2a2f828cbb3b84030838a
abdumannab-usmanov <usmanovabdumannab2307@gmail.com>
2022-06-07 22:37:47 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var journey = prompt("Money that you have for journey");
+
+
+
+var dollar = 750;
+var euro = 120;
+var usbDol = 11000;
+var usbEv = 11800;
+
+var resultDol = (dollar * usbDol );
+var resultEvr = (euro * usbEv );
+
+var totalResult = resultDol + resultEvr;
+
+
+
+if (journey >= totalResult){
+    console.log(`“Oq yo’l, Alisher!” `);
+}else{
+    console.log(`“Alisher, ozgina sabr qilish kerak bo’lar ekan.”`);
+}
